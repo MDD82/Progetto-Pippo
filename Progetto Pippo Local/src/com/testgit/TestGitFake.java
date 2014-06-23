aad287d0d54d78f40365535a0b32850b081c6c63
mauro.di.dio@accenture.com <mauro.di.dio@accenture.com>
2014-06-23 15:52:27 +0200

    Aggiunto "Per release 3"

--- Progetto Pippo Local/src/com/testgit/TestGitFake.java
+++ Progetto Pippo Local/src/com/testgit/TestGitFake.java
@@ -4,6 +4,7 @@ public class TestGitFake {
 	public static void main(String[] args) {
 		System.out.println("Ciao");
 		System.out.println("A tutti");
+		System.out.println("per la release 3");
 	}
 
 }
